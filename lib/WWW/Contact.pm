f0fc28fe6165ad318987455ace6d15b498afd794
fayland <fayland@gmail.com>
2008-10-27 12:10:39 +0000

    0.11 for my mistake

--- lib/WWW/Contact.pm
+++ lib/WWW/Contact.pm
@@ -4,7 +4,7 @@ use Class::MOP ();
 use Moose;
 use Moose::Util::TypeConstraints;
 
-our $VERSION   = '0.10';
+our $VERSION   = '0.11';
 our $AUTHORITY = 'cpan:FAYLAND';
 
 has 'errstr'   => ( is => 'rw', isa => 'Maybe[Str]' );
