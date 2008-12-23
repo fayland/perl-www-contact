2b9688e2f45adb0877706847ed1ad6826b928c49
fayland <fayland@gmail.com>
2008-12-23 01:34:17 +0000

    WWW-Contact-0.18.tar.gz

--- lib/WWW/Contact.pm
+++ lib/WWW/Contact.pm
@@ -4,7 +4,7 @@ use Class::MOP ();
 use Moose;
 use Moose::Util::TypeConstraints;
 
-our $VERSION   = '0.17';
+our $VERSION   = '0.18';
 our $AUTHORITY = 'cpan:FAYLAND';
 
 has 'errstr'   => ( is => 'rw', isa => 'Maybe[Str]' );
