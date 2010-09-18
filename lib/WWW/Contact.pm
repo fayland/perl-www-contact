8b89abd034c7e3f4d5e44cdd589b1c532f6987e4
Fayland Lam <fayland@gmail.com>
2010-09-18 09:25:02 +0800

    Build.pl and changes

--- lib/WWW/Contact.pm
+++ lib/WWW/Contact.pm
@@ -3,7 +3,7 @@ package WWW::Contact;
 use Class::MOP ();
 use Moose;
 
-our $VERSION   = '0.42';
+our $VERSION   = '0.43';
 our $AUTHORITY = 'cpan:FAYLAND';
 
 has 'errstr'   => ( is => 'rw', isa => 'Maybe[Str]' );
