d655ea8f4a39dea7d2b41f5908470c2718f23ba7
Fayland Lam <fayland@gmail.com>
2011-10-27 12:19:20 +0800

    fix AOL

--- lib/WWW/Contact.pm
+++ lib/WWW/Contact.pm
@@ -3,7 +3,7 @@ package WWW::Contact;
 use Class::MOP ();
 use Moose;
 
-our $VERSION   = '0.46';
+our $VERSION   = '0.47';
 our $AUTHORITY = 'cpan:FAYLAND';
 
 has 'errstr'   => ( is => 'rw', isa => 'Maybe[Str]' );
